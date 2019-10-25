c38b2fec5cbca387f9e971eda23cfa51043c530e
Nelson Neto <nelson.ash@outlook.com>
2019-10-25 20:48:41 -0300

    Criar App com usuários básicos

---
+++ NotificadorBG.Forms/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tulpep.NotificationWindow;
+
+namespace NotificadorBG.Forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            //Validar o usuário
+
+            //Conectar com HUB
+            //exibir mensagem
+            //Dar intervalo
+            var popupNotifier = new PopupNotifier();
+            popupNotifier.ImagePadding = new Padding(10);
+            popupNotifier.Image = Properties.Resources.infoIcon_32;
+            popupNotifier.TitleText = "Title of popup";
+            popupNotifier.ContentText = "Content text";
+            popupNotifier.IsRightToLeft = false;
+            popupNotifier.Popup();
+            base.OnLoad(e);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //var popupNotifier = new PopupNotifier();
+            //popupNotifier.ImagePadding = new Padding(10);
+            //popupNotifier.Image = Properties.Resources.infoIcon_32;
+            //popupNotifier.TitleText = "Title of popup";
+            //popupNotifier.ContentText = "Content text";
+            //popupNotifier.IsRightToLeft = false;
+            //popupNotifier.Popup();
+        }
+    }
+}
